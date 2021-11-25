d948b1f530ae267bacfe4661daad450702fb2a16
RyokoFJBYS <ryokofujibayashi828@gmail.com>
2021-11-25 13:32:43 +0000

    [Add]コントローラー・ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+  resources :posts
+  
+  resources :items
+
+  resources :users, only: [:index, :edit] do
+    member do
+      get   'unsubscribe'
+      patch 'withdraw'
+    end
+  end
   devise_for :users
+
+  root to: 'homes#top'
+  get "about" => "homes#about"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
