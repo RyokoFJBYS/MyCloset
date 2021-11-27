766b6f9856a2f963b24173b4ae332ea5ff905589
RyokoFJBYS <ryokofujibayashi828@gmail.com>
2021-11-27 14:48:48 +0000

    [Add]ユーザー編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_path
   end
 
   def unsubscribe
+    @user = current_user
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :status)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  enum status:{released: 0, nonreleased: 1}
 
 end
