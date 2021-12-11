b012795e790e6a08fb85e9195c463e01948fe78b
RyokoFJBYS <ryokofujibayashi828@gmail.com>
2021-12-11 04:42:32 +0000

    [Add]SignIn/SignUpレイアウト

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,29 +1,29 @@
 class FavoritesController < ApplicationController
-  
+
+  def create
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: post.id)
+    favorite.save
+    redirect_to post_path(post)
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite.destroy
+    redirect_to post_path(post)
+  end
+
   # def create
-  #   post = Post.find(params[:post_id])
-  #   favorite = current_user.favorites.new(post_id: post.id)
-  #   favorite.save
-  #   redirect_to post_path(post)  
-  # end
-  
-  # def destroy
-  #   post = Post.find(params[:post_id])
-  #   favorite = current_user.favorites.find_by(post_id: post.id)
-  #   favorite.destroy
+  #   @favorite = Favorite.new(user_id: @current_user.id, post_id: params[:post_id])
+  #   @favorite.save
   #   redirect_to post_path(post)
   # end
-  
-  def create
-    @favorite = Favorite.new(user_id: @current_user.id, post_id: params[:post_id])
-    @favorite.save
-    redirect_to post_path(post)
-  end
-  
-  def destroy
-    @favorite = Favorite.find_by(user_id: @current_user.id, post_id: params[:post_id])
-    @post.destroy
-    redirect_to post_path(post)
-  end
-  
+
+  # def destroy
+  #   @favorite = Favorite.find_by(user_id: @current_user.id, post_id: params[:post_id])
+  #   @post.destroy
+  #   redirect_to post_path(post)
+  # end
+
 end
